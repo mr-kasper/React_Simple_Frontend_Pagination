55e7facabce372d3b7c5bf9c262f59c191ce46a4
mr-kasper <rachidwork5050@gmail.com>
2024-09-17 12:17:14 +0100

    fix pagination issue

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -10,7 +10,7 @@ const Pagination = ({ postsPerPage, totalPosts, paginate }) => {
       <ul className="pagination">
         {pageNumbers.map((number) => (
           <li key={number} className="page-item">
-            <a href="!#" className="page-link" onClick={() => paginate(number)}>
+            <a href="#" className="page-link" onClick={() => paginate(number)}>
               {number}
             </a>
           </li>
